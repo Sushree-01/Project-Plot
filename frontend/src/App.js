556163ef6243640c63bd0e15e19bc4b49bdf62a8
Shivendra-Dixit <112815087+Shivendra-Pratap-Dixit@users.noreply.github.com>
2023-12-15 22:04:09 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,13 @@
 
 
 import './App.css';
-import Project from './Components/NavComponents/Project/Project';
 import Navroutes from './Mainroutes/Navroutes';
 function App() {
   return (
     <div className="App">
-      {/* <Navbar/> */}
+   
       <Navroutes/>
-      <Project/>
+     
     </div>
   );
 }
